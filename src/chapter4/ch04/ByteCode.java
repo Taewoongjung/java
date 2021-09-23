0382162d38491e015a649eba7c9fbfe7c01f5840
Taewoong <aipooh8882@naver.com>
2021-09-23 22:19:08 +0900

    today's commit

---
+++ src/chapter4/ch04/ByteCode.java
@@ -0,0 +1,13 @@
+package chapter4.ch04;
+
+public class ByteCode {
+    public static void main(String[] args) {
+
+        int A = 10;
+        int B = 20;
+        int C = 30;
+        int D = 40;
+
+        System.out.println(A = B + C * D);
+    }
+}
